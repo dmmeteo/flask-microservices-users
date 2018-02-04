7fca1c740e37ac874504484fabd93e6fb4ac954c
Dmitry Sichkar <dmmeteo@gmail.com>
2018-02-05 01:01:14 +0200

    add tests

---
+++ project/tests/test_users.py
@@ -0,0 +1,17 @@
+import json
+
+from project.tests.base import BaseTestCase
+
+
+class TestUserServise(BaseTestCase):
+    """Test for the Users Servise"""
+
+    def test_users(self):
+        """Ensure the /ping route bekaves correctly"""
+        response = self.client.get('/ping')
+        data = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('pong!', data['message'])
+        self.assertIn('success', data['status'])
+
+
