d9b19dbd3d8824d699082dc9f140bfd335be0fc8
Dmitry Sichkar <dmmeteo@gmail.com>
2018-02-06 21:31:28 +0200

    add get_single_user view & tests

--- project/tests/test_users.py
+++ project/tests/test_users.py
@@ -1,6 +1,9 @@
 import json
 
+from project import db
 from project.tests.base import BaseTestCase
+from project.api.models import User
+
 
 
 class TestUserServise(BaseTestCase):
@@ -81,6 +84,39 @@ class TestUserServise(BaseTestCase):
             self.assertEqual(response.status_code, 400)
             self.assertIn('Sorry. That email already exists.', data['message'])
             self.assertIn('fail', data['status'])
+
+    def test_single_user(self):
+        """Ensure get single user behaves correctly"""
+        user = User(username='boba', email='boba@realpython.com')
+        db.session.add(user)
+        db.session.commit()
+        with self.client:
+            response = self.client.get(f'/users/{user.id}')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 200)
+            self.assertTrue('created_at' in data['data'])
+            self.assertIn('boba', data['data']['username'])
+            self.assertIn('boba@realpython.com', data['data']['email'])
+            self.assertIn('success', data['status'])
+
+    def test_single_user_no_id(self):
+        """Ensure error is thrown if an id is not provided"""
+        with self.client:
+            response = self.client.get('/users/hui')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 404)
+            self.assertIn('User does not exist', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_single_user_incorrect_id(self):
+        """Ensure error is thrown if the id does not exist"""
+        with self.client:
+            response = self.client.get('/users/999')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 404)
+            self.assertIn('User does not exist', data['message'])
+            self.assertIn('fail', data['status'])
+ 
 
 
 
