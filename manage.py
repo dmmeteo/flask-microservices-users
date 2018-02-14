2162af54b82c0aa488224b1eef2abaa39a43e232
Dmitry Sichkar <dmmeteo@gmail.com>
2018-02-14 18:27:03 +0200

    add coverage

--- manage.py
+++ manage.py
@@ -1,9 +1,22 @@
 import unittest
+import coverage
+
 from flask_script import Manager
 
 from project import create_app, db
 from project.api.models import User
 
+
+COV = coverage.coverage(
+    branch=True,
+    include='project/*',
+    omit=[
+        'project/tests/*',
+        'project/server/config.py',
+        'project/server/*/__init__.py'
+    ]
+)
+COV.start()
 
 app = create_app()
 manager = Manager(app)
@@ -34,6 +47,23 @@ def test():
         return 0
     return 1
 
+@manager.command
+def cov():
+    """Runs the unit tests with coverage"""
+    tests = unittest.TestLoader().discover('project/tests/')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        COV.html_report()
+        COV.erase()
+        return 0
+    return 1
+
 
 if __name__ == '__main__':
     manager.run()
+
+
