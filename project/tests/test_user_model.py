4ec1b8f33b9fa713420beb7fdb46732460c7e918
Dmitry Sichkar <dmmeteo@gmail.com>
2018-02-27 23:52:04 +0200

    tests to models

---
+++ project/tests/test_user_model.py
@@ -0,0 +1,22 @@
+from project import db
+from project.tests.base import BaseTestCase
+from project.api.models import User
+
+class TestUserModel(BaseTestCase):
+    """Ensure that user is add to database"""
+    def test_add_user(self):
+        user = User(
+            username='justatest',
+            email='test@test.com'
+        )
+        db.session.add(user)
+        db.session.commit()
+        self.assertTrue(user.id)
+        self.assertTrue(user.active)
+        self.assertTrue(user.created_at)
+        self.assertEqual(user.username, 'justatest')
+        self.assertEqual(user.email, 'test@test.com')
+
+
+
+
